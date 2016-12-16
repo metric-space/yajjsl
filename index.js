0c5fc96e8fb38e40296f522d13f07c298f88bfaf
luke <nerorevenge@gmail.com>
2016-12-16 00:25:03 -0500

    Second iteration  - added tuples, as tuples are now the validation output instead of    strings - basic type arrays now can be validated

--- index.js
+++ index.js
@@ -1,15 +1,12 @@
 const M = require("monet");
 const R = require("ramda");
+const tuples = require('fantasy-tuples'),
+    Tuple2 = tuples.Tuple2;
 
-function validateType(type, value, key) {
-    return (R.type(value) == type) ?
-        M.Either.Right(value) :
-        M.Either.Left(key + " is not of expected type :" + type);
-}
 
 function checkIfEmpty(value, key) {
     return R.isEmpty(value) ?
-        M.Either.Left(key + " is empty") :
+        M.Either.Left(Tuple(key,"empty")) :
         M.Either.Right(value);
 }
 
@@ -17,9 +14,21 @@ function keysWithSchema(object, schema) {
     return R.pickBy((v, k) => !R.isNil(schema[k]), object);
 }
 
-function validateArray(array, type) {
-    return R.compose(R.all((x) => x == type),
-        R.map(R.type))(array);
+function validateArray(type, value, key) {
+    return (R.compose(R.all((x) => x == type),
+            R.map(R.type))(value)) ?
+        M.Either.Right(value) :
+        M.Either.Left(Tuple2(key,false));
+}
+
+function validateType(type, value, key) {
+    if (len(type) == 2) {
+        return validateArray(type[1], value, key);
+    }
+
+    return (R.type(value) == type) ?
+        M.Either.Right(value) :
+        M.Either.Left(Tuple2(key,false));
 }
 
 function validate(object, schema) {
@@ -36,7 +45,7 @@ function validate(object, schema) {
         const validationE = R.curry(checkIfEmpty)(R.__, key);
 
         return (R.isNil(value) ?
-                M.Either.Left(key + " is undefined") :
+                M.Either.Left(Tuple2(key, "undefined")) :
                 M.Either.Right(value))
             .bind(x => validationT(x))
             .bind(x => validationE(x))
@@ -45,7 +54,7 @@ function validate(object, schema) {
             }, function(x) {
                 if (["Object"].indexOf(R.type(x)) > -1) {
                     return validate(x, vschema["schema"]);
-                } 
+                }
 
                 return [];
             })
