e4583709f7c8e94629d2a7e4fc2e1b71c9cf21de
luke <nerorevenge@gmail.com>
2016-12-19 15:20:00 -0500

    Code overhaul  - complex array types now supported  - lots of mutually recursive functions  - no trampoline-ing so far, so blowing up the stack is a possibility  - type signature put in as comments, but not 100% correct

--- index.js
+++ index.js
@@ -3,34 +3,81 @@ const R = require("ramda");
 const tuples = require('fantasy-tuples'),
     Tuple2 = tuples.Tuple2;
 
+// Schema Rules
+//
+//  Schema:: Map String String
+//  Schema:: Map String ArrayX
+//
+//  ArrayX -> [Const String] or [Const Schema]  
+//   where value of const is either "Array"
+//
 
+
+// :: a -> String -> Either (String, String) a
 function checkIfEmpty(value, key) {
     return R.isEmpty(value) ?
         M.Either.Left(Tuple2(key, "empty")) :
         M.Either.Right(value);
 }
 
-function validSchemaKeys(schema) {
-    const keys = R.keys(schema);
-    return R.filter(function(x) {
-            const value = schema[x];
-            return !(R.isNil(value) || R.isEmpty(value) || (R.type(value) == "Object"))
-        },
-        keys)
+// :: Array[a] -> Bool
+function validArrayX(arrayX) {
+    if (!(arrayX.length == 2)) {
+        return false;
+    }
+    const val1 = arrayX[0];
+    const type2 = R.type(arrayX[1]);
+    const truthy = (val1 == "Array") && ["Object", "String"].indexOf(type2) > -1;
+    return type2 == "Object" ? truthy && validSchema(arrayX[1]) : truthy;
 }
 
+// :: Schema -> Either String Schema
+function validSchema(schema) {
+    const vals = R.values(schema);
+    return R.reduce(function(acc, x) {
+        const notEmpty = !(R.isNil(x) || R.isEmpty(x)) 
+	const rightType = ["Object", "String", "Array"].indexOf(R.type(x)) > -1;
+	const truthy = notEmpty && rightType;
+
+        if (truthy && R.type(x) == "Array") {
+            return acc && validArrayX(x);
+        } else if (truthy && R.type(x) == "Object"){
+	    return acc && validSchema(x);
+	}
+
+        return acc && truthy;
+
+    }, true, vals);
+}
+
+// :: SchemaFragment -> [a] -> String -> Either Tuple(String, String) [a]
 function validateArray(type, value, key) {
+    if (R.type(type) == "Object") {
+        const result = R.reduce(function(acc, x) {
+            const output = validate(x, type);
+            return acc.concat(output);
+        }, [], value);
+        return (R.flatten(result) == []) ? M.Either.Right(output) :
+            M.Either.Left(result);
+    }
+
     return (R.compose(R.all((x) => x == type),
             R.map(R.type))(value)) ?
         M.Either.Right(value) :
         M.Either.Left(Tuple2(key, false));
 }
 
+// :: SchemaFragment -> a -> String -> Either Tuple(String, String) [a]
 function validateType(type, value, key) {
     if (R.type(type) == "Array" &&
-        type.length == 2 &&
         R.type(value) == type[0]) {
         return validateArray(type[1], value, key);
+
+    } else if (R.type(type) == "Object"){
+	const output = validate(value, type);
+	return (output == [])? M.Either.Right(value) :
+           M.Either.Left(output);
+
     }
 
     return (R.type(value) == type) ?
@@ -38,9 +85,11 @@ function validateType(type, value, key) {
         M.Either.Left(Tuple2(key, false));
 }
 
+// :: JSON -> Schema -> [(String, String)]
 function validate(object, schema) {
 
-    const keys = validSchemaKeys(schema);
+    // assuming schema is valid 
+    const keys = R.keys(schema);
 
     return R.flatten(R.map(function(key) {
         const type = schema[key];
@@ -57,8 +106,8 @@ function validate(object, schema) {
             .cata(function(x) {
                 return [x];
             }, function(x) {
-                if (R.type(x) == "Object" && R.type(x) == type[0]) {
-                    return validate(x, type[1]);
+                if (R.type(x) == "Object") {
+                    return validate(x, type);
                 }
 
                 return [];
@@ -72,5 +121,6 @@ module.exports = {
     validateType: validateType,
     checkIfEmpty: checkIfEmpty,
     validateArray: validateArray,
-    validSchemaKeys: validSchemaKeys
+    validArrayX: validArrayX,
+    validSchema: validSchema
 }
